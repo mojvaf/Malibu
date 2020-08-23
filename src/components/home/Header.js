ef372f66dee2bf50148c0dc789d02a57748e8773
mojvaf <mvafa44@gmail.com>
2020-08-23 19:45:53 +0200

    first commit

---
+++ src/components/home/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Hero from '../globals/Hero'
+import aboutImg from '../../images/aboutBcg.jpeg'
+const Header = () => {
+    return (
+        <Hero img={aboutImg} >xxx</Hero>
+    )
+}
+export default Header
